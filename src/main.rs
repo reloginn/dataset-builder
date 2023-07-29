230f22b1cacdc4f103aab536506c5b7f0947fd7d
unknown <nikita.malina23@gmail.com>
2023-07-29 20:01:20 +0300

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,25 @@ use app_args::parse_args;
 
 mod app_args;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
     let args = parse_args().expect("Cannot parse args");
     let input_path = args.input;
     let output_path = args.output;
     let mut iter = 0;
-    let entries = std::fs::read_dir(input_path).expect("Cannot read directory");
-    for entry in entries.flatten() {
-        let path = entry.path();
-        if let "webp" | "png" | "jpg" | "jpeg" = path.extension().unwrap().to_str().unwrap() {
-            let file = std::fs::read(path.clone()).expect("Cannot read file");
+    std::fs::read_dir(input_path)?.flatten().for_each(|entry| {
+        if let "png" | "jpg" | "jpeg" = entry.path().extension().unwrap().to_str().unwrap() {
+            let file = std::fs::read(entry.path()).expect("Cannot read file");
             iter += 1;
             std::fs::write(
                 output_path
                     .join(format!("{}", iter))
-                    .with_extension(path.extension().expect("Cannot get extension")),
+                    .with_extension(entry.path().extension().expect("Cannot get extension")),
                 file,
             )
             .expect("Cannot write an image");
             std::fs::File::create(output_path.join(format!("{}", iter)).with_extension("txt"))
                 .expect("Cannot create file");
         }
-    }
+    });
+    Ok(())
 }
